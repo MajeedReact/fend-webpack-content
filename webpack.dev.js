08a6f5ad9a821f4031290f3c492193a400ed0e8a
majeedreact <mjalsalhi@gmail.com>
2022-12-02 23:06:44 +0300

    webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const htmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
+    mode: 'development',
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
@@ -17,6 +19,15 @@ module.exports = {
         new htmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
